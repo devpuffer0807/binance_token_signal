8d8f729d86c7e44085fb8ff850778b50f47af602
puffer <pufferdev0807@gmail.com>
2022-10-03 01:09:17 +0900

    fix

--- backend/common/db.js
+++ backend/common/db.js
@@ -1,3 +1,12 @@
 const { default: mongoose } = require("mongoose");
 
-mongoose.connect("mongodb+srv://doadmin:Wlve97qZ5D1068b4@db-mongodb-nyc1-24591-37cbcb59.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-24591");
+mongoose
+  .connect(
+    "mongodb+srv://doadmin:Wlve97qZ5D1068b4@db-mongodb-nyc1-24591-37cbcb59.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-24591"
+  )
+  .then(function (result, second) {
+    console.log("mongodb connect success");
+  })
+  .catch(function (err) {
+    console.log("Error", err);
+  });
