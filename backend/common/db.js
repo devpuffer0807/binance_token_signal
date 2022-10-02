aba8d8bc997fc96e5fc94dcef162071c038632da
puffer <pufferdev0807@gmail.com>
2022-10-03 01:02:50 +0900

    deploy fix

--- backend/common/db.js
+++ backend/common/db.js
@@ -1,3 +1,3 @@
 const { default: mongoose } = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/article_system");
+mongoose.connect("mongodb+srv://doadmin:Wlve97qZ5D1068b4@db-mongodb-nyc1-24591-37cbcb59.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-24591");
