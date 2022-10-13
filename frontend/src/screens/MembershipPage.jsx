99bc05f5104ea53680b35c65429cc9366a1feadd
Puffer <pufferdev0807@gmail.com>
2022-10-13 23:28:13 +0900

    update membership design

--- frontend/src/screens/MembershipPage.jsx
+++ frontend/src/screens/MembershipPage.jsx
@@ -7,95 +7,56 @@ import {
   Button,
   Spinner,
   Image,
+  Card,
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useAuth } from "../config/AuthProvider";
 import axios from "axios";
 import { SERVER_URL } from "../config";
 import { toast } from "react-toastify";
-import ImageUploader from "react-image-upload";
 import "react-image-upload/dist/index.css";
 
 export default function MembershipPage() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [nickName, setNicktName] = useState("");
-  const [userEmail, setUserEmail] = useState("");
-  const [userPassword, setUserPassword] = useState("");
-  const [userPhoto, setUserPhoto] = useState("");
-  const [firstNameVal, ] = useState(true);
-  const [lastNameVal, ] = useState(true);
-  const [nickNameVal, ] = useState("");
-  const [userEmailVal, ] = useState(true);
-  const [userPasswordVal, ] = useState(true);
-  const [isLoding, setIsLoding] = useState(false);
-
-  const { user } = useAuth();
-
-  useEffect(() => {
-    setFirstName(user.firstName);
-    setLastName(user.lastName);
-    setNicktName(user.nickName);
-    setUserEmail(user.userEmail);
-    setUserPhoto(user.photo);
-  }, []);
-
-  const getImageFileObject = (imageFile) => {
-    console.log(imageFile.file);
-    const formData = new FormData();
-    formData.append("file", imageFile.file);
-
-    axios
-      .post(SERVER_URL + "/upload", formData)
-      .then((res) => {
-        setUserPhoto(res.data.data);
-      })
-      .catch((err) => {});
-  };
-
-  const runAfterImageDelete = (file) => {
-    console.log({ file });
-  };
-
-  const saveProfile = () => {
-    setIsLoding(true);
-    const data = {
-      firstName: firstName,
-      lastName: lastName,
-      nickName: nickName,
-      userEmail: userEmail,
-      userPassword: userPassword,
-      photo: userPhoto,
-    };
-
-    axios
-      .post(SERVER_URL + "/profile/update", data)
-      .then((res) => {
-        if (res.data.status === true) {
-          setTimeout(() => {
-            setIsLoding(false);
-            toast.success(res.data.message);
-          }, 1000);
-        } else {
-          toast.error(res.data.message);
-          setTimeout(() => {
-            setIsLoding(false);
-            toast.error(res.data.message);
-          }, 2000);
-        }
-      })
-      .catch((err) => {
-        setTimeout(() => {
-          setIsLoding(false);
-          toast.error("Please check your login information!");
-        }, 2000);
-      });
-  };
-
   return (
-    <div className="MembershipPage">
+    <div className="MembershipPage pt-5 mt-5">
       <Container className="col-md-8 col-lg-8 col-sm-10">
-        MembershipPage
+        <div className="fs-1 fw-bold text-center">
+          Pricing
+        </div>
+        <div className="fs-5-2 text-center mt-2">
+          You can choose one of our subscription plans and start tracking signals.
+        </div>
+        <Row className="mt-5 pt-2">
+          <Col md={6} sm={12}>
+            <Card className="text-center"
+              border='info'
+
+            >
+              <Card.Header>1 Month Subscription</Card.Header>
+              <Card.Body className="p-5">
+                  <Card.Title>71 USDT/Mo</Card.Title>
+                  <Card.Text>
+                    30 days subscription
+                  </Card.Text>
+                  <Button variant="info" className="mt-4 w-100 p-2">Get Start</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={6} sm={12}>
+            <Card className="text-center"
+              border='danger'
+            >
+              <Card.Header>3 Month Subscription</Card.Header>
+              <Card.Body className="p-5">
+                  <Card.Title>131 USDT/Mo</Card.Title>
+                  <Card.Text>
+                    90 days subscription
+                  </Card.Text>
+                  <Button variant="primary" className="mt-4 w-100 p-2">Get Start</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
