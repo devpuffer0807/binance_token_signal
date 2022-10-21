c165d65f0117ce3185d76d96e529b67561e80073
Puffer <pufferdev0807@gmail.com>
2022-10-21 22:42:17 +0900

    update membership page frontend

--- frontend/src/screens/MembershipPage.jsx
+++ frontend/src/screens/MembershipPage.jsx
@@ -15,6 +15,7 @@ import axios from "axios";
 import { SERVER_URL } from "../config";
 import "react-image-upload/dist/index.css";
 import { Navigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
 const MEMBERSHIP_PLAN = 
   {
@@ -78,27 +79,52 @@ const SelectUSDTPage = (props) => {
   return (
     <div className="SelectUSDTPage by-5">
         <div>
-          <div className="">Send USDT to Whaler Hunter App Wallet</div>
-          <div className="text-danger fs-5 fw-semibold">SELECT USDT</div>
+          <div className="mb-3 fs-6 fw-semibold text-primary">Send USDT to Whaler Hunter App Wallet</div>
+          <div className="mb-3 text-primary-1 fs-6 fw-semibold">SELECT USDT</div>
           <Image src="https://whalehuntapp.com/assets/whitdraw.png"/>
         </div>
-      <Button onClick={() => {props.setPage(2)}}>Next</Button>
+        <div className="d-flex justify-content-end">
+          <Button className="bg-primary-1 text-white border border-0 me-3" onClick={() => {props.setPage(0)}}>Previous</Button>
+          <Button className="bg-primary-1 text-white border border-0" onClick={() => {props.setPage(2)}}>Next</Button>
+        </div>
     </div>
   );
 }
 const CopyWalletAddress = (props) => {
   return (
     <div className="CopyWalletAddress">
-      3rd page
-      <Button onClick={() => {props.setPage(3)}}>nesldkfj</Button>
+      <div>
+          <div className="mb-3 fs-6 fw-semibold text-primary">Submit Your Subscription Fees</div>
+          <div className="mb-3 text-primary-1 fs-6 fw-semibold">SEND ONLY 71.00 USDT !NEITHER MORE NOR LESS!</div>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label><small className="fw-semibold text-primary">Copy This Address</small></Form.Label>
+            <Form.Control type="text" value="TU5hm4GB1s5JcnoxzF5nv4gzpZ4AWopSHn" readOnly/>
+          </Form.Group>
+          <Image src="https://whalehuntapp.com/assets/tron.jpg"/>
+        </div>
+        <div className="d-flex justify-content-end">
+          <Button className="bg-primary-1 text-white border border-0 me-3" onClick={() => {props.setPage(1)}}>Previous</Button>
+          <Button className="bg-primary-1 text-white border border-0" onClick={() => {props.setPage(3)}}>Next</Button>
+        </div>
     </div>
   )
 }
 const InputTransferCode = (props) => {
   return (
     <div className="InputTransferCode">
-      4th page
-      <Button onClick={() => {props.setPage(3)}}>next</Button>
+      <div>
+          <div className="mb-3 fs-6 fw-semibold text-primary">Paste Transfer Code When Transfer Completed</div>
+          <div className="mb-3 fs-6 fw-semibold text-primary">Check Wallet > Deposit & Witdrawal History</div>
+          <Image src="https://whalehuntapp.com/assets/transfercode.png"/>
+          <Form.Group className="my-3" controlId="formBasicEmail">
+            <Form.Label><small className="fw-semibold text-primary">Transfer Code</small></Form.Label>
+            <Form.Control type="text" placeholder="Transfer Code" onChange={(e) => props.setTransfercode(e.target.value)}/>
+          </Form.Group>
+        </div>
+        <div className="d-flex justify-content-end">
+          <Button className="bg-primary-1 text-white border border-0 me-3" onClick={() => {props.setPage(2)}}>Previous</Button>
+          <Button className="bg-primary-1 text-white border border-0" onClick={props.submitHandler}>Submit</Button>
+        </div>
     </div>
   )
 }
@@ -106,30 +132,55 @@ export default function MembershipPage() {
   const { user } = useAuth();
   const [page, setPage] = useState(0);
   const [curplan, setCurplan] = useState("");
-
+  const [transfercode, setTransfercode] = useState("");
   if (!user) {
     return <Navigate to="/login" />;
+  }
+  const membershipSubmitHandler = () => {
+    if(transfercode == "") {
+      toast.error("Please input transfercode.");
+      return;
+    }
+    let payload = {
+      plan: curplan,
+      transferCode: transfercode,
+      user: user  
+    };
+    axios({
+      method: 'post',
+      url: SERVER_URL + '/saveproposal',
+      data: payload
+    })
+    .then((response) => {
+      const recvData = response.data;
+      if(!recvData.status) {
+        console.error(recvData.message);
+        return;
+      }
+      else {
+        console.log(recvData);
+      }
+    }).catch((e) => console.error(e));
   }
 
   return (
     <>
+    <ToastContainer />
       {
         page != 0 && (
-          <div className="membership-title container py-5">
+          <div className="container py-5">
             <Card>
               <Card>
                 <Card.Body>
-                  <Card.Title className="mb-3">
+                  <Card.Title className="mb-3 text-primary fs-6 fw-semibold">
                     { MEMBERSHIP_PLAN[curplan].CONTENT }
                   </Card.Title>
-                  <Card.Text className="mb-5">
+                  <Card.Text className="mb-5 text-warning fs-6 fw-semibold">
                     If your request is correct, it will be approved within 12 hours. To learn the TxId code, you can follow the instruction athttps://www.binance.com/en/support/faq/2c325e53daf04442adbaf8f6ba052f71. Incorrect requests will be denied.
                   </Card.Text>
                     {
-                      page == 0 ? (
-                        <MembershipPlanPage setPage={setPage} setCurplan={setCurplan}/>
-                      ) : page == 1 ? <SelectUSDTPage setPage={setPage} planKey={curplan}/> : page == 2 ? <CopyWalletAddress setPage={setPage} />
-                      : <InputTransferCode setPage={setPage} />
+                      page == 1 ? <SelectUSDTPage setPage={setPage} planKey={curplan}/> : page == 2 ? <CopyWalletAddress setPage={setPage} />
+                      : <InputTransferCode setPage={setPage} curplan={curplan} transfercode={transfercode} setTransfercode={setTransfercode} submitHandler={membershipSubmitHandler}/>
                     }
                 </Card.Body>
               </Card>
@@ -137,7 +188,9 @@ export default function MembershipPage() {
           </div>
         )
       }
-      
+      {
+        page == 0 && (<MembershipPlanPage setPage={setPage} setCurplan={setCurplan}/>)
+      }
       
     </>
   );
